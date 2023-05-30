47eb8993a12b637d8e204f29a58951ecdf36565b
Yurii Korolyk <yura.korolyk@gmail.com>
2023-05-30 20:31:01 +0300

    hw-1

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
-const fs = require('node:fs/promises')
-console.log(fs)
+const fs = require('fs/promises');
+const path = require('path');
+
+(async () => {
+    try {
+        await fs.mkdir(path.join(__dirname, 'folder_main'))
+        for (let i = 0; i < 5; i++) {
+           await fs.mkdir(path.join(__dirname, 'folder_main', `folder${i+1}`))
+           await fs.writeFile(path.join(__dirname, 'folder_main', `file${i+1}.txt`), `file number ${i+1}`)
+        }
+        await readDir()
+    }
+    catch (err) {
+        throw new Error(err.message)
+    }
+})();
+const readDir = async () =>  {
+    try {
+        const data = await fs.readdir(path.join(__dirname, 'folder_main'), { withFileTypes: true });
+        data.forEach(item => {
+            if (item.isDirectory()) {
+                console.log(`FOLDER: ${item.name}`);
+            } else {
+                console.log(`FILE: ${item.name}`);
+            }
+        });
+
+    } catch (err) {
+        throw new Error(err.message);
+    }
+};
+
+
